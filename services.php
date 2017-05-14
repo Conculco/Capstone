908dde6d25905fc6b14bd8dca0865bdf5cad47fb
Stephen Downer <conculco.sd@gmail.com>
2017-05-14 23:50:01 +1000

    Starting the mockup

---
+++ services.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<link rel="stylesheet" href="styles.css">
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+<div id="wrapper">
+<?php
+	include 'header.html';
+?>
+<div id="banner"></div>
+<div id="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ullamcorper lacus sodales luctus elementum. Morbi tincidunt massa vel felis lacinia consequat. Sed sed augue pellentesque, elementum ipsum vel, aliquam ipsum. Etiam a aliquam purus. Proin fermentum arcu at nibh rhoncus, non viverra tellus feugiat. Cras aliquet auctor fermentum. Morbi fringilla nisl lectus, ac molestie ligula egestas vel. Cras ac dui tristique sapien elementum congue. Nam eu magna in metus pharetra congue sed nec enim. Nullam eu ex eros. Nulla venenatis tortor viverra purus imperdiet volutpat. Donec tincidunt, sapien consectetur vestibulum volutpat, nunc nisl porttitor tellus, quis posuere urna sapien eu ante. Etiam quis lacus sit amet augue auctor tempus at ut lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed facilisis hendrerit metus, sit amet lacinia neque eleifend vitae. Duis eu imperdiet ligula.</div>
+</div>
+</body>
+</html>
