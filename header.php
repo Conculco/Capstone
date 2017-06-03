0aaa53002553386cc0320c256c612cd521cb56e5
Unknown <Conculco.sd@gmail.com>
2017-06-04 03:20:12 +1000

    Log Out and Small fixes

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
 <div class="Logo"><a href="index.php"><img src="img/logo.png" width="150" height="130"></a></div>
+<div class="Logout"><form action="logout.php"><input type="submit" value="Logout"></form></div>
 <div class="Header"></div>
 <div class="Header2"></div>
---
+++ logout.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Lovey Pets</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="5; url=login.php" />
+</head>
+<body>
+<?php
+    include 'header.php';
+?>
+<div class="center">
+    <div class="centertext">
+        <h1>Logout Successful</h1>
+    </div>
+</div>
+
+</body>
+</html>
