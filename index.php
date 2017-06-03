b639dd4f362096d69bc3511aaa3d495f6df18a59
Unknown <Conculco.sd@gmail.com>
2017-06-04 00:25:47 +1000

    Getting there

--- index.php
+++ index.php
@@ -1,17 +1,52 @@
 ﻿<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<link rel="stylesheet" href="styles.css">
-    <meta charset="utf-8" />
-    <title></title>
-</head>
+<html>
+<title>Lovey Pets</title>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    .Logo {
+        float: left;
+        margin: 10px;
+
+    }
+    .Header {
+        height: 100px;
+        background-color: gray;
+    }
+    Whitespace
+    .Whitespace {
+        width: 100%;
+        height: 5%;
+        background-color: gray;
+    }
+    .container > div {
+        display: inline-block;
+        zoom: 1; /* Trigger hasLayout */
+        width: 25%;
+        text-align: center;
+    }
+</style>
 <body>
-<div id="wrapper">
-<?php
-	include 'header.html';
-?>
-<div id="banner"></div>
-<div id="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ullamcorper lacus sodales luctus elementum. Morbi tincidunt massa vel felis lacinia consequat. Sed sed augue pellentesque, elementum ipsum vel, aliquam ipsum. Etiam a aliquam purus. Proin fermentum arcu at nibh rhoncus, non viverra tellus feugiat. Cras aliquet auctor fermentum. Morbi fringilla nisl lectus, ac molestie ligula egestas vel. Cras ac dui tristique sapien elementum congue. Nam eu magna in metus pharetra congue sed nec enim. Nullam eu ex eros. Nulla venenatis tortor viverra purus imperdiet volutpat. Donec tincidunt, sapien consectetur vestibulum volutpat, nunc nisl porttitor tellus, quis posuere urna sapien eu ante. Etiam quis lacus sit amet augue auctor tempus at ut lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed facilisis hendrerit metus, sit amet lacinia neque eleifend vitae. Duis eu imperdiet ligula.</div>
+
+<div class="w3-container w3-gray">
+
+
+</div>
+<div class="Logo"><img src="img/logo.png" width="150" height="130"></div>
+<div class="Header"></div>
+<div class="Whitespace">test</div>
+<div class="Whitespace">test</div>
+<div class="Whitespace">test</div>
+<div class="container">
+    <!–- LEAVE THIS AS 1 ugly line –->
+    <div><a href="report.php"><img src="img/Screen.png" width="200" height="200"></div></a><div><a href="walkin.php"><img src="img/bone.png" width="200" height="200"></a></div><div><a href="bookings.php"><img src="img/dog cat.png" width="200" height="200"></a></div><div><a href="stock.php"><img src="img/syringe.png" width="200" height="200"></a></div>
+</div>
+<div class="container">
+    <!–- LEAVE THIS AS 1 ugly line –->
+    <div>Reports</div><div>Walk In Customer</div><div>Bookings</div><div>Inventroy</div>
 </div>
 </body>
 </html>
