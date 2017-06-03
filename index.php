cf00e9b3fff7accba203f3529d5cbca461471058
Unknown <Conculco.sd@gmail.com>
2017-06-04 01:29:28 +1000

    Header Fixed

--- index.php
+++ index.php
@@ -1,45 +1,14 @@
 ﻿<!DOCTYPE html>
 <html>
-<title>Lovey Pets</title>
-
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-    }
-    .Logo {
-        float: left;
-        margin: 10px;
-
-    }
-    .Header {
-        height: 100px;
-        background-color: gray;
-    }
-    Whitespace
-    .Whitespace {
-        width: 100%;
-        height: 5%;
-        background-color: gray;
-    }
-    .container > div {
-        display: inline-block;
-        zoom: 1; /* Trigger hasLayout */
-        width: 25%;
-        text-align: center;
-    }
-</style>
+<head>
+    <title>Lovey Pets</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta charset="UTF-8">
+</head>
 <body>
-
-<div class="w3-container w3-gray">
-
-
-</div>
-<div class="Logo"><img src="img/logo.png" width="150" height="130"></div>
-<div class="Header"></div>
-<div class="Whitespace">test</div>
-<div class="Whitespace">test</div>
-<div class="Whitespace">test</div>
+<?php
+    include 'header.php';
+?>
 <div class="container">
     <!–- LEAVE THIS AS 1 ugly line –->
     <div><a href="report.php"><img src="img/Screen.png" width="200" height="200"></div></a><div><a href="walkin.php"><img src="img/bone.png" width="200" height="200"></a></div><div><a href="bookings.php"><img src="img/dog cat.png" width="200" height="200"></a></div><div><a href="stock.php"><img src="img/syringe.png" width="200" height="200"></a></div>
