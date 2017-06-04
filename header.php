24b4c891bd1efbe7f19a06ae01dc4c129d533a9b
Joshua Stacey <jstacey1109@gmail.com>
2017-06-04 12:14:41 +1000

    New Design
    Signed-off-by: Joshua Stacey <jstacey1109@gmail.com>

--- header.php
+++ header.php
@@ -1,4 +1,15 @@
 <div class="Logo"><a href="index.php"><img src="img/logo.png" width="150" height="130"></a></div>
+  <div id="branding">
+    <h1>Lovely Pets</h1>
+  </div>
+  <nav>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="contact.html">Support</a></li>
+        <li><a href="staff.html">Staff</a></li>
+        <li><a href="reception.html">Reception</a></li>
+    </ul>
+  </nav>
 <div class="Logout"><form action="logout.php"><input type="submit" value="Logout"></form></div>
 <div class="Header"></div>
 <div class="Header2"></div>
