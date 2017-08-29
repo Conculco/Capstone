fc7eaa512877740306d24b91e3b78e833803c363
Stephen Downer <conculco.sd@gmail.com>
2017-08-30 04:15:08 +1000

    Reverteed some changes for demo

--- create/petscript.php
+++ create/petscript.php
@@ -1,28 +1,29 @@
 <body>
-    <h1>New Pet Created</h1>
+<h1>New Pet Created</h1>
 </body>
 
 
 <?php
-  include("../config.php");
+include("../config.php");
 try {
-	$statement = $conn->prepare("INSERT INTO Pet (customer_id, treatment_id, petname, dob, sex, species, breed, description, microchip_id)
-								VALUES(:customer_id, :treatment_id, :petname, :dob, :sex, :species, :breed, :description, :microchip_id)");
-														
-	$statement->execute(array(
-	"customer_id" => $_POST['customer_id'],
-    "treatment_id" => $_POST['treatment_id'],
-    "petname" => $_POST['petname'],
-	"dob" => $_POST['dob'],
-	"sex" => $_POST['sex'],
-	"species" => $_POST['species'],
-	"breed" => $_POST['breed'],
-	"description" => $_POST['description'],
-    "microchip_id" => $_POST['microchip_id']
-));
+    $statement = $conn->prepare("INSERT INTO Pet (customer_id, petname, dob, sex, species, breed, description, microchip_id)
+								VALUES(:customer_id, :petname, :dob, :sex, :species, :breed, :description, :microchip_id)");
+
+    $statement->execute(array(
+        "customer_id" => $_POST['customer_id'],
+        "petname" => $_POST['petname'],
+        "dob" => $_POST['dob'],
+        "sex" => $_POST['sex'],
+        "species" => $_POST['species'],
+        "breed" => $_POST['breed'],
+        "description" => $_POST['description'],
+        "microchip_id" => $_POST['microchip_id']
+    ));
+    $id = $_POST['customer_id'];
+    header("Location: ../customer.php?id=$id");
+    die();
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
 ?>
-
--- createpet.php
+++ createpet.php
@@ -1,54 +1,51 @@
-﻿<?php
+﻿<!DOCTYPE html>
+<?php
 session_start();
 ?>
-<!DOCTYPE html>
 <html>
 
 <head>
     <title>Lovey Pets</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
-    <script src="http://malsup.github.com/jquery.form.js"></script>
-    <script src="js/main.js"></script>
 </head>
 
 <body>
 <?php
-    include 'header.php';
+include 'header.php';
 ?>
-    <div class="center">
-        <form name="form" id="form">
-            <label>customer_id</label>
-            <input id="customer_id" type="text" value="<?php echo $_GET['id'];?>">
+<div class="center">
+    <form action="create/petscript.php" method="post">
+        <label>customer_id</label>
+        <input name="customer_id" type="text" value="<?php echo $_GET['id'];?>">
 
-			<label>Pet Name</label>
-            <input id="petname" type="text" placeholder="">
+        <label>Pet Name</label>
+        <input name="petname" type="text" placeholder="">
 
-			<label>Date of Birth</label>
-            <input id="dob" type="text" placeholder="">
+        <label>Date of Birth</label>
+        <input name="dob" type="text" placeholder="">
 
-			<label>Sex</label>
-            <input id="sex" type="text" placeholder="Male/Female">
+        <label>Sex</label>
+        <input name="sex" type="text" placeholder="Male/Female">
 
-			<label>Species</label>
-            <input id="species" type="text" placeholder="Cat/Dog">
+        <label>Species</label>
+        <input name="species" type="text" placeholder="Cat/Dog">
 
-			<label>Breed</label>
-            <input id="breed" type="text" placeholder="">
+        <label>Breed</label>
+        <input name="breed" type="text" placeholder="">
 
-			<label>Description</label>
-            <input id="description" type="text" placeholder="description">
+        <label>Description</label>
+        <input name="description" type="text" placeholder="description">
 
-			<label>Microchip_id</label>
-            <input id="microchip_id" type="text" placeholder="microchip_id">
+        <label>microchip_id</label>
+        <input name="microchip_id" type="text" placeholder="microchip_id">
 
-            <input type="button" id="submit" onclick="createPetFunction()" value="Create Pet">
-        </form>
-    </div>
+        <input type="submit" value="Create Pet">
+    </form>
+</div>
 <div class="whitespace"> </div>
 <?php
-    include 'footer.php';
+include 'footer.php';
 ?>
 </body>
 
