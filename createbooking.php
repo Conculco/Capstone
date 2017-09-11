82a543233783ba9c139bb9bd89a9caa666ba7b82
Unknown <jstacey1109@gmail.com>
2017-09-11 20:48:09 +1000

    Create Booking -
    Created booking, using time date now, css revisions needed

--- createbooking.php
+++ createbooking.php
@@ -6,10 +6,13 @@ session_start();
 
 <head>
     <title>Lovey Pets</title>
-    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
-    <script src="http://malsup.github.com/jquery.form.js"></script>
+    <!-- Minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Minified JS library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Minified Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
 </head>
 <body>
@@ -17,25 +20,36 @@ session_start();
     include 'header.php';
 ?>
     <div class="center">
-        <form id="form" name="form">
-			      <label>pet_id</label>
+        <form method="post" action="">
+            <label>pet_id</label>
             <input id="pet_id" type="text" value="<?php echo $_GET['pet_id'];?>">
 
 			      <label>clinic_id</label>
             <input id="clinic_id" type="text" value="1">
 
-            <label>bookingType</label>
-            <input id="bookingType" type="text" placeholder="bookingType">
+            <label>Booking Type</label>
+            <input id ="bookingType" type="text" name="booking_type" class="form-control">
 
-			      <label>time</label>
-            <input id="time" type="text" placeholder="time">
-
-			      <label>date</label>
-            <input id="date" type="text" placeholder="2017-08-02">
+            <label>Date Time</label>
+            <input size="16" type="text" name="event_datetime" class="form-control" id="dateTime" readonly>
 
             <input id="submit" type='button' onclick="bookingFunction()" value="Create Booking">
         </form>
-    </div>
+</div>
+
+<script src="js/bootstrap-datetimepicker.min.js"></script>
+<script type="text/javascript">
+$(function () {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes();
+    var dateTime = date+' '+time;
+$("#dateTime").datetimepicker({
+        format: 'yyyy-mm-dd hh:ii'
+      });
+    });
+</script>
+
 <div class="whitespace"> </div>
 <?php
     include 'footer.php';
--- js/main.js
+++ js/main.js
@@ -78,3 +78,53 @@ alert(html);
 }
 return false;
 }
+
+$("#datepicker").datepicker().change(function() {
+  	var $this = $(this);
+    var data = {
+      json: JSON.stringify({
+        array: [ {id: 1,text: "8 AM - 10 AM"} , {id: 2,text: "10 AM - 12 AM"} ]
+      }),
+      delay: 0
+    };
+    $.ajax({
+      url:"/echo/json/",
+      data:data,
+      type:"POST",
+      success:function(response) {
+        checkin(response);
+      }
+    });
+  });
+function checkin(data) {
+  var content = "";
+  for (var arr=data.array, idx=0; idx < arr.length; idx++) {
+  	content += "<tr class='text-center'><td colspan=7><button id="+arr[idx].id+">"+arr[idx].text+"</button></td></tr>";
+  }
+  $("#datepicker").find('.ui-datepicker-current-day').parent()
+  .after(content);
+}
+
+$(".ui-state-default").on("mouseover", function() {
+    var $this = $(this);
+    var data = {
+      json: JSON.stringify({
+        count: ""+Math.floor((Math.random() * 10) + 1),
+      }),
+      delay: 0
+    };
+    $.ajax({
+      url:"/echo/json/",
+      data:data,
+      type:"POST",
+      success:function(response) {
+        availability($this, response);
+      }
+    });
+  });
+
+function availability($obj, data){
+	$obj.attr("title", data.count + " Available");
+  $obj.data("toggle", "tooltip");
+  $obj.tooltip();
+}
