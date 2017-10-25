211b652aa0571864bc10076e9d16edf8e87cb90a
Stephen Downer <conculco.sd@gmail.com>
2017-10-25 11:32:42 +1000

    Moveing stuff around

--- website/FindaVet.php
+++ website/FindaVet.php
@@ -22,7 +22,7 @@ p.big {
 </style> 
 <body>
 <header>
-    <div class="Logo"><a href="index.php"><img src="img/logo.png" width="100" height="50"></a>
+    <div class="Logo"><a href="index_2.php"><img src="img/logo.png" width="100" height="50"></a>
     </div>
     <div id="branding">
         <h1>Lovely <span class="highlight">Pets</span></h1>
@@ -30,11 +30,11 @@ p.big {
 
     <nav>
         <ul>
-            <li class="current"><a href="index.php">Home</a></li>
-            <li><a href="aboutus.html">About Us</a></li>
+            <li class="current"><a href="index_2.php">Home</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
             <li><a href="findavet.php">Find a Vet</a></li>
-            <li><a href="ContactUs.html">Contact Us</a></li>
-            <li><a href="reception.php">Inventory</a></li>
+            <li><a href="ContactUs.php">Contact Us</a></li>
+            <li><a href="services.php">Services</a></li>
         </ul>
     </nav>
 </header>
@@ -47,12 +47,13 @@ p.big {
 
 <p class= "small"> Lovely Pets Veterinary offers exceptional client and patient care giving you peace of mind that when in our care, your pet will be treated like family.<br/>
 
+
+
+
 <p>To find your local vet enter your post code below: </p>
 
 
-<div class="footer">
-    <p>Lovely Pets, Copyright &copy; 2017</p>
-</div>
+
 
 <?php
     include 'footer.php';
