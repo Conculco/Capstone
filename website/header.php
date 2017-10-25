816cae733860a39cc7e0f7125842ecd12755e879
Stephen Downer <conculco.sd@gmail.com>
2017-10-25 22:52:38 +1000

    Website Links

---
+++ website/header.php
@@ -0,0 +1,18 @@
+
+<header>
+    <div class="Logo"><a href="index.php"><img src="img/logo.png" width="100" height="50"></a>
+    </div>
+    <div id="branding">
+        <h1>Lovely <span class="highlight">Pets</span></h1>
+    </div>
+
+    <nav>
+        <ul>
+            <li class="current"><a href="index.php">Home</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="findvet.php">Find a Vet</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+            <li><a href="services.php">Services</a></li>
+        </ul>
+    </nav>
+</header>
--- website/index.php
+++ website/index.php
@@ -23,23 +23,9 @@ p.big {
 </style>
 
 <body>
-<header>
-    <div class="Logo"><a href="index.php"><img src="img/logo.png" width="100" height="50"></a>
-    </div>
-    <div id="branding">
-        <h1>Lovely <span class="highlight">Pets</span></h1>
-    </div>
-
-    <nav>
-        <ul>
-           <li class="current"><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About Us</a></li>
-            <li><a href="Findavet.php">Find a Vet</a></li>
-            <li><a href="ContactUs.php">Contact Us</a></li>
-            <li><a href="services.php">Services</a></li>
-        </ul>
-    </nav>
-</header>
+<?php
+include 'header.php';
+?>
 
 <h1> Welcome to Lovely Pets Veterinary  </h1>
 
