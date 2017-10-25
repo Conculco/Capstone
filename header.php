24807025deea95da919b19cefd5e4387f441666e
Stephen Downer <conculco.sd@gmail.com>
2017-10-25 10:17:29 +1000

    Revert "Logo Fix"
    This reverts commit 8be284db67eaa8c7184558edfcbcf12749996273.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 include 'access_control.php';
 ?>
 <header>
-    <div class="Logo"><a href="index.php"><img src="/img/logo.png" width="100" height="50"></a>
+    <div class="Logo"><a href="index.php"><img src="img/logo.png" width="100" height="50"></a>
     </div>
     <div id="branding">
         <h1>Lovely <span class="highlight">Pets</span></h1>
