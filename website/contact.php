037498c5aa5b05424d444bfc0b691eb8b3524540
Stephen Downer <conculco.sd@gmail.com>
2017-10-25 22:59:01 +1000

    Merge Fixed

--- website/contact.php
+++ website/contact.php
@@ -10,24 +10,9 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-<header>
-    <div class="Logo"><a href="index.php"><img src="img/logo.png" width="100" height="50"></a>
-    </div>
-    <div id="branding">
-        <h1>Lovely <span class="highlight">Pets</span></h1>
-    </div>
-
-    <nav>
-        <ul>
-            <li class="current"><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About Us</a></li>
-            <li><a href="FindaVet.php">Find a Vet</a></li>
-            <li><a href="ContactUs.php">Contact Us</a></li>
-            <li><a href="services.php">Services</a></li>
-
-        </ul>
-    </nav>
-</header>
+<?php
+include 'header.php';
+?>
 
 <h1> Contact Us </h1>
 
--- website/findvet.php
+++ website/findvet.php
@@ -22,30 +22,9 @@ p.big {
 }
 </style>
 <body>
-<<<<<<< HEAD
 <?php
 include 'header.php';
 ?>
-=======
-<header>
-    <div class="Logo"><a href="index.php"><img src="img/logo.png" width="100" height="50"></a>
-    </div>
-    <div id="branding">
-        <h1>Lovely <span class="highlight">Pets</span></h1>
-    </div>
-
-    <nav>
-        <ul>
-            <li class="current"><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About Us</a></li>
-            <li><a href="FindaVet.php">Find a Vet</a></li>
-            <li><a href="ContactUs.php">Contact Us</a></li>
-            <li><a href="services.php">Services</a></li>
-        </ul>
-    </nav>
-</header>
->>>>>>> master
-
 <h1> Find a Vet </h1>
 
 <font size= "4">
